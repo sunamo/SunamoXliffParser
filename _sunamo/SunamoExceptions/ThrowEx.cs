aa0da0da9ca3420107d2a9aabec6bf412d3b6a5e
sunamo.cz <radek.jancik@sunamo.cz>
2024-11-09 10:38:27 +0100

    Fix throwing exception

--- _sunamo/SunamoExceptions/ThrowEx.cs
+++ _sunamo/SunamoExceptions/ThrowEx.cs
@@ -51,7 +51,7 @@ internal partial class ThrowEx
 
     internal static bool ThrowIsNotNull(string? exception, bool reallyThrow = true)
     {
-        if (exception == null)
+        if (exception != null)
         {
             Debugger.Break();
             if (reallyThrow)
